346f0169e00b348b449eac0621f1f3fb88de8bb5
Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
2014-07-25 19:46:46 +0200

    rpi-userland: No need for staging cleanup
    The files added by default to the staging folder by the install are harmless, so
    keep them and simplify the make rules of this package.
    
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>

--- package/raspberrypi/rpi-userland/rpi-userland.mk
+++ package/raspberrypi/rpi-userland/rpi-userland.mk
@@ -13,12 +13,6 @@ RPI_USERLAND_INSTALL_TARGET = YES
 
 RPI_USERLAND_PROVIDES = libegl libgles libopenmax libopenvg
 
-define RPI_USERLAND_POST_STAGING_CLEANUP
-    rm -Rf $(STAGING_DIR)/opt/vc/{bin,sbin}
-    rm -f  $(STAGING_DIR)/etc/init.d/vcfiled
-    rmdir -p $(STAGING_DIR)/etc/init.d || true
-endef
-
 define RPI_USERLAND_POST_TARGET_CLEANUP
     rm -Rf $(TARGET_DIR)/opt/vc/include
     rm -Rf $(TARGET_DIR)/opt/vc/share
@@ -29,8 +23,6 @@ define RPI_USERLAND_POST_TARGET_CLEANUP
     grep -q "/opt/vc/lib" $(TARGET_DIR)/etc/ld.so.conf || \
         echo "/opt/vc/lib" >> $(TARGET_DIR)/etc/ld.so.conf
 endef
-
-RPI_USERLAND_POST_INSTALL_STAGING_HOOKS += RPI_USERLAND_POST_STAGING_CLEANUP
 RPI_USERLAND_POST_INSTALL_TARGET_HOOKS  += RPI_USERLAND_POST_TARGET_CLEANUP
 
 $(eval $(cmake-package))
