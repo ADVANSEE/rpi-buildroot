47de94698f1227ac53ec069f28c8afd60ce7057e
Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
2014-07-25 19:32:55 +0200

    rpi-firmware: Fix config naming breakage
    BR2_PACKAGE_RPI_FIRMWARE was used instead of BR2_TARGET_RPI_FIRMWARE, preventing
    some config options from being available. Most config options also had the same
    naming error in their prefix.
    
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>

--- boot/rpi-firmware/rpi-firmware.mk
+++ boot/rpi-firmware/rpi-firmware.mk
@@ -11,8 +11,8 @@ RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
 
 define RPI_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/boot/bootcode.bin $(BINARIES_DIR)/boot/bootcode.bin
-	$(INSTALL) -D -m 0644 $(@D)/boot/start$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).elf $(BINARIES_DIR)/boot/start.elf
-	$(INSTALL) -D -m 0644 $(@D)/boot/fixup$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).dat $(BINARIES_DIR)/boot/fixup.dat
+	$(INSTALL) -D -m 0644 $(@D)/boot/start$(BR2_TARGET_RPI_FIRMWARE_BOOT).elf $(BINARIES_DIR)/boot/start.elf
+	$(INSTALL) -D -m 0644 $(@D)/boot/fixup$(BR2_TARGET_RPI_FIRMWARE_BOOT).dat $(BINARIES_DIR)/boot/fixup.dat
 	$(INSTALL) -D -m 0644 boot/rpi-firmware/config.txt $(BINARIES_DIR)/boot/config.txt
 endef
 
