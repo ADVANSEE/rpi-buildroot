aa82f8ed92608aaf56beb9e0a2a6fec72bc5f292
Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
2014-08-13 16:17:31 +0200

    rpi: Add support for HTTP server
    Install an HTTP server with PHP support, but do not start it by default.
    
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>

--- board/raspberrypi/postscript.sh
+++ board/raspberrypi/postscript.sh
@@ -6,6 +6,10 @@ TARGET="${1}"
 [ -f ${TARGET}/etc/init.d/S80dhcp-server ] &&
 		mv -f ${TARGET}/etc/init.d/S80dhcp-server ${TARGET}/etc/init.d/_S80dhcp-server
 
+# do not start the HTTP server by default
+[ -f ${TARGET}/etc/init.d/S50lighttpd ] &&
+		mv -f ${TARGET}/etc/init.d/S50lighttpd ${TARGET}/etc/init.d/_S50lighttpd
+
 # copy System.map
 cp ${TARGET}/../build/linux-*/System.map ${TARGET}/System.map
 
